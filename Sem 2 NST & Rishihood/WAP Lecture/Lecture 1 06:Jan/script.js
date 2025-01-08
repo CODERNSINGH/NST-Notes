11a8a2dfcf1171430edb3262c1b4f62de35f5f51
CODERNSINGH <narendrasingh.coder@gmail.com>
2025-01-08 09:51:41 +0530

    day3

---
+++ Sem 2 NST & Rishihood/WAP Lecture/Lecture 1 06:Jan/script.js
@@ -0,0 +1,50 @@
+var a = "11";
+var b = 1;
+var c = true;
+var d = 0;
+var e = "hi";
+
+
+console.log(a+b);
+console.log(a-b);
+
+var name = "Narendra Singh";
+console.log(name + " Student at Rishihood");
+
+console.log(name + 'Nothing to Write');
+console.log(name + 'Doing some shit');
+console.log(a,b,c,d,e);
+
+let college = "Newton school"
+let Best_college = "Scaler School of technology"
+
+console.log(college ,"<<", Best_college)
+
+const x = "time"
+// var x = false
+
+
+console.log(x)
+
+// Decleration 
+var firstName; //Decleration
+firstName = "NSN" //initialization
+
+
+var current_college = "Newton School"
+current_college = "Rishihood University" //re-dclear
+
+
+
+// let current_college "Newon School"  //cannot re-dclear but can
+// console.log(current_college)  
+
+let cu_col = "Newton School"
+cu_col = "Rishihood University" //re-assign possible
+console.log("re-assigned value"+ cu_col)
+
+
+// const current_college = "Newton School of Technology"
+current_college = "RU"
+
+console.log(current_college)
---
+++ Sem 2 NST & Rishihood/WAP Lecture/Lecture 2 08:jan/script.js
@@ -0,0 +1,22 @@
+// ways to make string
+
+let collegeName = "Newton school of technology"
+let studentName = 'Narendra Singh'
+
+//way 3 = Backtick(string tamplate literals)
+let friendName = `Keshav`
+
+let massage =  `Ohh sad ${studentName} you are student of ${collegeName}`
+
+console.log(massage)
+
+// Methods of string
+let Country = 'India' //['I', 'n', ... like this]
+
+console.log(Country[0]) // accessing charector
+// and if no value gives undefined
+
+
+//Getting length of string
+console.log(Country.length)
+
