c5a5722c07649d97aee92a5c759c2ce17eb28731
CODERNSINGH <narendrasingh.coder@gmail.com>
2025-02-13 19:39:31 +0530

    13 feb

--- Sem 2 NST & Rishihood/WAP Lecture/Lecture 11 10:feb/index.js
+++ Sem 2 NST & Rishihood/WAP Lecture/Lecture 11 10:feb/index.js
@@ -1,3 +1,16 @@
-// Promise in JS
+// Promise in JS by codewith harry
 
-const promise = new Promise()
+let promise = new Promise(function(resolve, reject){
+    // alert("i am alert in promise")
+    resolve(56)
+})
+
+
+console.log("Hello 1")
+
+setTimeout(() => {
+    console.log("Hello 2")
+}, 1000);
+
+console.log("Hello 3")
+console.log(promise)
--- Sem 2 NST & Rishihood/WAP Lecture/Lecture 12 12:Feb/index.js
+++ Sem 2 NST & Rishihood/WAP Lecture/Lecture 12 12:Feb/index.js
@@ -1,2 +1,27 @@
 // Understanding api's
+const api = `https://jsonplaceholder.typicode.com/users`
+const responce = fetch(api)
 
+console.log(responce)
+
+responce.then((res) =>{
+    res.json().then((data)=>{
+        console.log(data)
+    })
+}).catch((error)=>{
+
+})
+
+
+// understanding
+
+function succses(value){
+    value.json().then(res =>{
+
+        console.log(value)
+    })
+}
+function error(value){
+    console.log(value)
+}
+responce.then(succses).catch(error)
