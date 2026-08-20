844ea6991fbae861d07048c946d3747b29721211
Narendra Singh <narendrasingh.coder@gmail.com>
2026-08-20 11:45:33 +0530

    updated some stuff

---
+++ Sem 5 NST & Rishihood/App Development/Day 06 20 Aug/sir_code.js
@@ -0,0 +1,39 @@
+const {readdirSync} = require('fs');
+const {spawn} = require("child_process")
+const {join} = require('path')
+
+
+let allSongs = [];
+let currentMusicIndex = 0;
+let cursor = 0;
+
+
+
+function listSongs(songDirPath){
+    const songDirPath = join(__dirname,'song')
+    allSongs = readdirSync(songDirPath)
+
+    allSongs.forEach((data,index) => {
+        console.log(`${cursor === index ? '>': ''} ${data}`)
+    });
+}
+
+function playSong(songPath){
+    spawn('afplay',[songPath])
+}
+
+process.stdin.on('data',data =>{
+    const final = data.toString();
+    if (data === '^[[A'){
+        listSongs()
+        return
+    }
+
+    if (data = '^[[B'){
+        // up arrow key
+        
+    }
+    
+
+
+})
